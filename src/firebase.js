162852120d9a403b6c41e2dfa4fe0f41a722e364
Mohamm2ed <131975893+Mohamm2ed@users.noreply.github.com>
2024-11-01 23:57:27 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBwYwR9SWOn1uFzMX3Krlg7egdgahLdyH8",
-  authDomain: "portfoloi-dfb46.firebaseapp.com",
-  projectId: "portfoloi-dfb46",
-  storageBucket: "portfoloi-dfb46.appspot.com",
-  messagingSenderId: "929884610951",
-  appId: "1:929884610951:web:74119a108e3c095ff4f322",
-  measurementId: "G-K533PYXXQ4"
+   
 };
 
 // Initialize Firebase
